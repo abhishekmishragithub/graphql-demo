5507e4366c91d8a409fe62ec5a176f326e2f955a
Abhishek Mishra <geekabhi@outlook.com>
2019-10-01 06:00:23 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,16 @@
+import requests
+
+
+q = """
+{ 
+  website(url: "https://in.pycon.org") {
+    title
+    image
+    description
+  }
+}
+"""
+
+
+resp = requests.post("http://localhost:5000/", params={'query': q})
+print(resp.text)
